cd1d1a7ad373de21071a26bb33dfc91564c3f351
Synk0 <wesleyhod.wh@gmail.com>
2017-07-04 05:13:43 -0300

    Using true to loop infinite

--- run_forever.sh
+++ run_forever.sh
@@ -2,8 +2,7 @@
 
 # This script keeps the bot running "forever", if it dies
 # the script will relaunch it
-RC=1
-while [ $RC -ne 0 ]; do
-   python3 myinstantsbot.py
-      RC=$?
-      done
+while true; do
+  python3 myinstantsbot.py
+  sleep 4s
+done
